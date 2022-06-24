91088a35ab04e00c0e7d4fc1e96d83d94a73f1fe
Wisteli-ShikhaPandey <85352745+Shikha-code36@users.noreply.github.com>
2022-06-24 18:10:08 +0530

    Best time to buy and sell stock

--- Arrays/121-Best-Time-To-Buy-and-Sell-Stock.py
+++ Arrays/121-Best-Time-To-Buy-and-Sell-Stock.py
@@ -1,27 +1,27 @@
 '''Leetcode - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/'''
 
-#Solution1
+# Solution1
 def maxProfit(prices):
-    maxp= 0
+    maxp = 0
 
     for i in range(len(prices)-1):
-        for j in range(i+1,len(prices)):
+        for j in range(i+1, len(prices)):
             profit = prices[j]-prices[i]
-            if profit>maxp:
-                maxp=profit
+            if profit > maxp:
+                maxp = profit
     return maxp
 
-#T:O(N^2)
-#S:O(1)
+# T:O(N^2)
+# S:O(1)
 
-#Solution2
+# Solution2
 def maxProfit(prices):
-    maxp= 0
+    maxp = 0
 
     l = 0
     for r in range(1, len(prices)):
         if prices[r] < prices[l]:
-                l = r
+            l = r
         maxp = max(maxp, prices[r] - prices[l])
     return maxp
 
