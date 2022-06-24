424b532a15604281a947ddede7fe4e8761828d96
Wisteli-ShikhaPandey <85352745+Shikha-code36@users.noreply.github.com>
2022-06-24 17:29:09 +0530

    Contains Duplicate

--- Arrays/217-Contains-duplicate.py
+++ Arrays/217-Contains-duplicate.py
@@ -3,7 +3,7 @@
 # Solution1
 def containsDuplicate(nums):
     for i in range(len(nums)):
-        for j in range(i+1,len(nums)):
+        for j in range(i+1, len(nums)):
             if nums[i] == nums[j]:
                 return True
             return False
@@ -24,3 +24,15 @@ def containsDuplicate(nums):
 
 # T: O(N)
 # S: O(N)
+
+# Solution3
+def containsDuplicate(nums):
+    nums.sort()
+
+    for i in len(1, nums):
+        if nums[i] == nums[i-1]:
+            return True
+        return False
+
+# T: O(nlogN)
+# S: O(1)
