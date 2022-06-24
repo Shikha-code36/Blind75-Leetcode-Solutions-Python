2a5af676ffac09cf170906f9d8e08f96d46eeb8a
Wisteli-ShikhaPandey <85352745+Shikha-code36@users.noreply.github.com>
2022-06-24 17:46:53 +0530

    Two sum

--- Arrays/001-twosum.py
+++ Arrays/001-twosum.py
@@ -1,23 +1,24 @@
 '''Leetcode - https://leetcode.com/problems/two-sum/ '''
 
-#Solution1
+# Solution1
 def twoSum(nums, target):
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                  if nums[i] + nums[j] == target:
-                        return [i,j]
-#T:O(N^2)
-#S:O(1)
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+# T:O(N^2)
+# S:O(1)
 
-#Solution2 hashmap
+# Solution2 hashmap
+
+
 def twoSum(nums, target):
     dict = {}
     for i in range(len(nums)):
         diff = target - nums[i]
         if diff in dict:
-          return [dict[diff], i]
+            return [dict[diff], i]
         dict[nums[i]] = i
 
 # T: O(N)
 # S: O(N)
-
---
+++ Arrays/53-maximum-subarray.py
@@ -0,0 +1,9 @@
+'''Leetcode -https://leetcode.com/problems/maximum-subarray/'''
+
+#Solution1
+#T:O(N^2)
+#S:O(1)
+
+#Solution1
+# T:O(N)
+# S:O(1)
