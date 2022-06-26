6fa84183ccef0595712c2d5182b477e95ec5fbbd
Wisteli-ShikhaPandey <85352745+Shikha-code36@users.noreply.github.com>
2022-06-26 18:21:36 +0530

    Maximum Subarray

--- Arrays/53-maximum-subarray.py
+++ Arrays/53-maximum-subarray.py
@@ -1,4 +1,12 @@
 '''Leetcode -https://leetcode.com/problems/maximum-subarray/'''
+'''
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+A subarray is a contiguous part of an array.
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+'''
 
 # Solution1
 import math
