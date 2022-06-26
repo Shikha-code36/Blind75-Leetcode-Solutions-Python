26343565680cb8e66c2198c2330e40d0376e4902
Wisteli-ShikhaPandey <85352745+Shikha-code36@users.noreply.github.com>
2022-06-26 18:24:10 +0530

    Contains Duplicate

--- Arrays/217-Contains-duplicate.py
+++ Arrays/217-Contains-duplicate.py
@@ -1,4 +1,10 @@
 '''Leetcode - https://leetcode.com/problems/contains-duplicate/'''
+'''
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Input: nums = [1,2,3,1]
+Output: true
+'''
 
 # Solution1
 def containsDuplicate(nums):
